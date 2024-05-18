128e949042db657bf2a21b9d171e223960b7cf9a
kaestro <didme654@snu.ac.kr>
2024-05-19 07:22:16 +0900

    prop은 promise가 있어서 컴포넌트에 두지 않는다.

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"i+jfQXUG8UaOwTK8HwktrHauT48kv9L7QAvLUI75Ys0=\"\n}"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
+import fs from 'fs'
+import matter from 'gray-matter'
+import { GetStaticProps } from 'next'
 import Link from 'next/link'
+import path from 'path'
 import React from 'react'
-import getStaticProps from './getStaticProps'
+import { getAllPosts, getCategories, getLatestPostsByCategory } from '../utils'
 
 const HomePage: React.FC<{ posts: { id: string, title: string, content: string }[], latestPostsByCategory: any[] }> = ({ posts, latestPostsByCategory }) => {
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-4">
       {latestPostsByCategory.map(({ category, posts }: { category: string, posts: { id: string, title: string }[] }) => (
-        <div key={category}>
-          <h2>{category}</h2>
+        <div key={category} className="p-4 border rounded-md">
+          <h2 className="text-xl font-bold">{category}</h2>
           <ul>
             {posts.map((post: { id: string, title: string })  => (
-              <li key={post.id}>
-                <Link href={`/posts/${post.id}`}>{post.title}</Link>
+              <li key={post.id} className="my-2">
+                <Link href={`/posts/${post.id}`} className="text-blue-500 hover:underline">{post.title}</Link>
               </li>
             ))}
           </ul>
@@ -21,5 +25,36 @@ const HomePage: React.FC<{ posts: { id: string, title: string, content: string }
   )
 }
 
-export { getStaticProps }
+export const getStaticProps: GetStaticProps = async () => {
+  const postsDirectory = path.join(process.cwd(), '_posts')
+  const filePaths = getAllPosts(postsDirectory)
+
+  const posts = filePaths.flatMap(filePath => {
+    const fileContents = fs.readFileSync(filePath, 'utf8')
+    const { data, content } = matter(fileContents)
+    const filename = path.basename(filePath)
+
+    return [{
+      id: filename.replace(/\.mdx?$|\.markdown$/, ''),
+      ...data,
+      date: data.date instanceof Date ?data.date.toISOString() :data.date, // Date 객체를 ISO 문자열로 변환
+    }]
+  })
+
+  const categories = getCategories(posts)
+
+  const latestPostsByCategory = categories.map(category => ({
+    category,
+    posts: getLatestPostsByCategory(posts, category),
+  }))
+
+  return {
+    props: {
+      posts,
+      categories,
+      latestPostsByCategory,
+    },
+  }
+}
+
 export default HomePage
